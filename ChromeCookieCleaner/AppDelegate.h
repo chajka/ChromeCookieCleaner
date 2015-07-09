e3251d484a777dad6a6ef071bc018f12ddeb9aeb
Чайка <mellanie335@gmail.com>
2015-07-10 00:59:22 +0900

    Initial Commit

---
+++ ChromeCookieCleaner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ChromeCookieCleaner
+//
+//  Created by Чайка on 7/10/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ChromeCookieCleaner/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ChromeCookieCleaner
+//
+//  Created by Чайка on 7/10/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
